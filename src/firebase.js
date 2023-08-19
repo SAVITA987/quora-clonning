8abf6042d205e8fdf30b9bdb2aab2ca0ddad25b0
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-08-19 09:21:16 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4AthTdDQFx8QcbHyD4V_mDkVvSfDC6kQ",
-  authDomain: "quora-clone-9ab4c.firebaseapp.com",
-  projectId: "quora-clone-9ab4c",
-  storageBucket: "quora-clone-9ab4c.appspot.com",
-  messagingSenderId: "617455548741",
-  appId: "1:617455548741:web:7dcdf571b0e11c12f2a62c",
-  measurementId: "G-JS7N2P77WS"
+  apiKey: "AIzaSyB4ZDCM2CQm-sXZxJQdH9ynsfpgEVVp46A",
+  authDomain: "quora-clone-c713d.firebaseapp.com",
+  projectId: "quora-clone-c713d",
+  storageBucket: "quora-clone-c713d.appspot.com",
+  messagingSenderId: "122219076232",
+  appId: "1:122219076232:web:a4f090f9f57a073595dd3d",
+  measurementId: "G-RN9N0MRPY1"
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 // firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
